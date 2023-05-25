0ee058372b74c8f6f91081b5050306e91b5413d5
ry <ry.scherbarth@gmail.com>
2023-05-24 21:35:58 -0500

    added home & resume pages, initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/resume'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'pages#home'
+  get '/resume', to: 'pages#resume'
 end
