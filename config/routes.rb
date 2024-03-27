2b14aa5eb9dee0e8cd526cc5eea69c5473a183a2
Ryan Scherbarth <ry.scherbarth@gmail.com>
2024-03-27 11:12:34 -0600

    adding zoom redirect

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   resources :pages
   resources :links
+
+  # route for zoom meeting link
+  # https://unm.zoom.us/my/ryans
+  get '/zoom', to: redirect('https://unm.zoom.us/my/ryans')
 end
