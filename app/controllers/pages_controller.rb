f3d63cb4ddef1ec8fd8d2322283f30c02b229ffe
Ryan Scherbarth <ry.scherbarth@gmail.com>
2024-08-04 20:51:49 -0600

    creating routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,31 +1,9 @@
 class PagesController < ApplicationController
-  layout "application"
-
   def index
     # logic for index
   end
 
   def show
     # logic for show
-  end
-
-  def new
-    # logic for new
-  end
-
-  def create
-    # logic for create
-  end
-
-  def edit
-    # logic for edit
-  end
-
-  def update
-    # logic for update
-  end
-
-  def destroy
-    # logic for destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   get 'about', to: 'pages#about'
-
+  get 'sc24', to: 'pages#sc24'
+  get 'courses', to: 'pages#courses'
 
   # route for zoom meeting link
   # https://unm.zoom.us/my/ryans
-  # get '/zoom', to: redirect('https://unm.zoom.us/my/ryans')
+  get '/zoom', to: redirect('https://unm.zoom.us/my/ryans')
 end
