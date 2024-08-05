a7a03c1d737b8616e3d2ffc77776ac5f41236c3c
Ryan Scherbarth <ry.scherbarth@gmail.com>
2024-08-04 20:04:19 -0600

    remove

--- config/application.rb
+++ config/application.rb
@@ -11,13 +11,9 @@ module Web
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.template_engine :haml
+    end
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
-
-
-
   # route for zoom meeting link
   # https://unm.zoom.us/my/ryans
   # get '/zoom', to: redirect('https://unm.zoom.us/my/ryans')
