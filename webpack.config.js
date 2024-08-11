aa08a9520d8d0d2d4c9bc713f6825dfe732001c4
Ryan Scherbarth <ry.scherbarth@gmail.com>
2024-08-11 18:08:24 -0500

    dependency issues

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './app/javascript/packs/application.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public', 'packs')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
