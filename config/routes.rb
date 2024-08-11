e2ab3789e9759ff7827231ff0070d4519e18c57f
Ryan Scherbarth <ry.scherbarth@gmail.com>
2024-08-11 13:02:42 -0500

    converted to resume page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   get 'meet', to: 'pages#meet'
 
   # route for dropbox resume pdf
-  get '/resume', to: redirect('https://www.dropbox.com/scl/fi/gvtppgbtwmum74o1j5a88/Ryan-Scherbarth-Resume.pdf?rlkey=vyygimwhisrypxhrwl948aey5&st=cx3tp0ls&dl=0')
+  # get '/resume', to: redirect('https://www.dropbox.com/scl/fi/gvtppgbtwmum74o1j5a88/Ryan-Scherbarth-Resume.pdf?rlkey=vyygimwhisrypxhrwl948aey5&st=cx3tp0ls&dl=0')
+  get '/resume', to: 'pages#resume'
 
   # route for zoom meeting link
   # https://unm.zoom.us/my/ryans
