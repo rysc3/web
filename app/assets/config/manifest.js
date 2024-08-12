0c369f178b80000876bae39b0fd93a5c828ff88f
Ryan Scherbarth <ry.scherbarth@gmail.com>
2024-08-11 21:21:58 -0500

    SC23 carosel

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -4,3 +4,9 @@
 //= link application.css
 //= link application1.js
 //- link_tree ../images//= link_tree ../builds
+//= link_tree ../images/SC23
+
+/*
+  Recompile the manifest file with the following command:
+  rake assets:precompile
+*/
