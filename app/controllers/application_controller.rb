71f0d1ee59147d194b9c835f4578f90e23e4984d
Ryan Scherbarth <ry.scherbarth@gmail.com>
2024-08-31 12:09:40 -0600

    define open graph images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   layout "application"
+
+  before_action :set_default_og_image
+
+  private
+
+  def set_default_og_image
+    @og_image = 'Profile-01.jpeg'
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
+  layout "application"
+
   def index
     # logic for index
   end
 
   def show
     # logic for show
+  end
+
+  def sc24
+    @og_image = "SC24-01.jpeg"
   end
 end
