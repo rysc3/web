ce21e624371fdef20556ab3a138592bb3e74eb34
Ryan Scherbarth <ry.scherbarth@gmail.com>
2024-08-31 12:40:43 -0600

    fixing page titles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@ class ApplicationController < ActionController::Base
   layout "application"
 
   before_action :set_default_og_image
+  before_action :set_default_page_title
 
   private
 
   def set_default_og_image
     @og_image = 'Profile-01.jpg'
+  end
+
+  def set_default_page_title
+    @page_title = "Ryan's Website"
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,10 +11,20 @@ class PagesController < ApplicationController
 
   def sc24
     @og_image = "SC24-01.jpeg"
+    @page_title = "Ryan @ SC24"
   end
 
   def sc23
     @og_image = "SC23-06.jpeg"
+    @page_title = "Ryan @ SC23"
+  end
+
+  def resume
+    @page_title = "Ryan's Resume"
+  end
+
+  def web
+    @page_title = "Ryan's Site Info"
   end
 
 end
