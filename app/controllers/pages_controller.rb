d10529e404e4e26c5e26fc370c41659959dea5dc
Ryan Scherbarth <ry.scherbarth@gmail.com>
2025-06-18 01:21:08 -0700

    breaking out templates for calc, chart. Fixing options calculation logic and adding new defaults

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  require 'net/http'
+  require 'json'
+  require 'uri'
   layout "application"
 
   def index
@@ -53,21 +56,48 @@ class PagesController < ApplicationController
   end
 
   def stock
-    @start     = params[:start].to_f
-    @end       = params[:end].to_f
-    @opt_pct   = params[:opt_pct].to_f / 100.0
+    @current_price = fetch_price
+    @end = params[:end].present? ? params[:end].to_f : @current_price
+    @start = params[:start].present? ? params[:start].to_f : 308.0
+    @opt_pct = (params[:opt_pct].presence || 0).to_f / 100.0
     @pkg_value = params[:package].to_f
 
     if @start.positive? && @end.positive?
-      rsu_pct = 1.0 - @opt_pct
-      delta = [@end - @start, 0].max
+      delta_pct = (@end - @start) / @start
 
-      initial_value = @start
-      current_value = rsu_pct * @end + @opt_pct * 3 * delta
-      @gain_pct = ((current_value - initial_value) / initial_value * 100).round(1)
+      option_gain = delta_pct * 3 * @opt_pct
 
-      @new_pkg_value = @pkg_value.positive? ? (@pkg_value * current_value / initial_value) : nil
+      rsu_gain = delta_pct * (1 - @opt_pct)
+
+      total_gain = option_gain + rsu_gain
+
+      current_value = @start * (1 + total_gain)
+
+      @gain_pct = (total_gain * 100).round(1)
+      @new_pkg_value = if @pkg_value.positive?
+        (@pkg_value * current_value / @start).round(2)
+      end
+      @break_even = @start
     end
+  end
+
+
+  private
+
+  def fetch_price
+    uri = URI("https://query1.finance.yahoo.com/v8/finance/chart/TSLA?range=1d&interval=1d")
+    request = Net::HTTP::Get.new(uri)
+    request["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(request)
+    end
+
+    data = JSON.parse(response.body)
+    data.dig("chart", "result", 0, "meta", "regularMarketPrice") || 0
+  rescue => e
+    Rails.logger.error("Failed to fetch price: #{e.message}")
+    0
   end
 
 end
