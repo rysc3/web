a19bac247b90d8823735367139cda5e6a21cdfc5
Ryan Scherbarth <ry.scherbarth@gmail.com>
2026-03-29 01:18:49 -0700

    remove stock and career

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -46,10 +46,6 @@ class PagesController < ApplicationController
     @page_title = "Ryan's Resume"
   end
 
-  def career
-    @page_title = "Ryan's Career"
-  end
-
   def web
     @page_title = "Ryan's Site Info"
   end
@@ -57,52 +53,6 @@ class PagesController < ApplicationController
   def tesla_battery
     @og_image = "tesla-battery.png"
     @page_title = "Ryan's sentry graph"
-  end
-
-  def stock
-    @og_image = "stock.jpg"
-    @page_title = "Ryan's Epic Stock Calculator"
-
-    @current_price = fetch_price
-    @end = params[:end].present? ? params[:end].to_f : @current_price
-    @start = params[:start].present? ? params[:start].to_f : 308.0
-    @opt_pct = (params[:opt_pct].presence || 0).to_f / 100.0
-    @pkg_value = params[:package].to_f
-
-    if @start.positive? && @end.positive?
-      delta_pct = (@end - @start) / @start
-
-      option_gain = delta_pct * 3 * @opt_pct
-      rsu_gain = delta_pct * (1 - @opt_pct)
-      total_gain_pct = option_gain + rsu_gain
-
-      @gain_pct = (total_gain_pct * 100).round(1)
-
-      if @pkg_value.positive?
-        @total_gain = (@pkg_value * total_gain_pct).round(2)
-      end
-
-      @break_even = @start
-    end
-  end
-
-
-  private
-
-  def fetch_price
-    uri = URI("https://query1.finance.yahoo.com/v8/finance/chart/TSLA?range=1d&interval=1d")
-    request = Net::HTTP::Get.new(uri)
-    request["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-
-    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-      http.request(request)
-    end
-
-    data = JSON.parse(response.body)
-    data.dig("chart", "result", 0, "meta", "regularMarketPrice") || 0
-  rescue => e
-    Rails.logger.error("Failed to fetch price: #{e.message}")
-    0
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,12 @@ Rails.application.routes.draw do
   get 'courses', to: 'pages#courses'
   get 'meet', to: 'pages#meet'
   get 'web', to: 'pages#web'
-  get 'stock', to: 'pages#stock'
-
   # Route to show /app/assets/images/tesla-battery.png
   get 'tesla_battery', to: 'pages#tesla_battery'
 
   # route for dropbox resume pdf
   # get '/resume', to: redirect('https://www.dropbox.com/scl/fi/gvtppgbtwmum74o1j5a88/Ryan-Scherbarth-Resume.pdf?rlkey=vyygimwhisrypxhrwl948aey5&st=cx3tp0ls&dl=0')
   get '/resume', to: 'pages#resume'
-  get '/career', to: 'pages#career'
 
   # route for zoom meeting link
   # https://unm.zoom.us/my/ryans
